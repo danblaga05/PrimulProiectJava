08622d62b46218c2030399c257414f2ae7c3c0a1
danblaga05 <blaga.dan.andrei@gmail.com>
2019-05-23 20:46:28 +0300

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    System.out.println(4);
+    System.out.println("salut");
+
+    char myChar = 'p';
+    byte byteI = 123;
+    short myShort = 12345;
+    short myInt = 1;
+    long myLong = 123465789789789l;
+    float myFloat = 12321;
+    String myString = "dan";
+    int result = myChar + 4;
+    System.out.println("rezultate:" + (2 + 2));
+
+    int x = 2;
+    x+=x;
+    //x = x + x;
+    x-=x;
+    //x = x - x;
+    x++;
+    //x + 1
+    x --;
+    // x - 1;
+        int y = 2;
+        System.out.println(++y);
+    System.out.println(y+=y+=y++ + ++y);
+
+    }
+}
